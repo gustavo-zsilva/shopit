1d63f76d8361169fbb3213293c82e42ebe416f31
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-09-21 17:22:15 -0300

    Added Web Folder && Initial website config

---
+++ web/src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    colors: {
+        bg: "#1A202C",
+    },
+    fonts: {
+        body: "Noto Sans, system-ui, sans-serif",
+        heading: "Noto Sans, system-ui, sans-serif",
+    }
+})
